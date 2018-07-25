eaec1334e3eac9434903f4eff54cf1f749381e72
vendettafv <leonardofeitosa.v@gmail.com>
2018-07-25 20:20:34 -0300

    Update Script

--- script.sh
+++ script.sh
@@ -21,8 +21,32 @@ if [ "$SERVER" == "1" ];
 then
 	# Apache
 	echo "Instalação do Apache2"
+  echo "Escolha a versão do PHP"
+  echo "1) PHP 5.6"
+  echo "2) PHP 7.1"
+  read PHPV1
+  if [ "$PHPV1" == "1" ]
+  then
+    # PHP 5.6
+	  echo "Instalação do PHP 5.6"
+  elif [ "$PHPV1" == "2" ]
+  then
+    # PHP PHP 7.1
+	  echo "Instalação do PHP PHP 7.1"
 elif [ "$SERVER" == "2" ];
 then
   # Apache
 	echo "Instalação do nginx"
+  echo "Escolha a versão do PHP"
+  echo "1) PHP 5.6 + PHP-FPM"
+  echo "2) PHP 7.1 + PHP-FPM"
+  read PHPV2
+  if [ "$PHPV2" == "1" ]
+  then
+    # PHP 5.6
+	  echo "Instalação do PHP 5.6 + PHP-FPM"
+  elif [ "$PHPV2" == "2" ]
+  then
+    # PHP PHP 7.1
+	  echo "Instalação do PHP PHP 7.1 + PHP-FPM"
 fi
