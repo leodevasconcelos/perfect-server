950712cf7a51415baa341120f30f46ac099058b2
vendettafv <leonardofeitosa.v@gmail.com>
2018-07-25 20:06:15 -0300

    Update Script

--- script.sh
+++ script.sh
@@ -1,12 +1,28 @@
 #!/bin/bash
+# Código OK
+# echo "####### Instalando MySQL Server #######"
+# sudo apt-get install mysql-server
 
-echo "####### Instalando MySQL Server #######"
-sudo apt-get install mysql-server
+# echo "Instalação segura do MySQL (y/n)?"
+# read ANS
+# if [ "$ANS" == "y" ] || [ "$ANS" == "Y" ];
+# then
+# 	# Instalação segura do MySQL
+# 	sudo mysql_secure_installation
+# fi
 
-echo "Instalação segura do MySQL (y/n)?"
+# Desenvolvimento de Teste
+
+echo "\e[1;31m Instalação do Web Service"
+echo "1) apache2"
+echo "2) nginx"
 read ANS
-if [ "$ANS" == "y" ] || [ "$ANS" == "Y" ];
+if [ "$ANS" == "1" ];
 then
-	# Instalação segura do MySQL
-	sudo mysql_secure_installation
+	# Apache
+	echo "Instalação do Apache2"
+elif [ "$ANS" == "2" ];
+then
+  # Apache
+	echo "Instalação do nginx"
 fi
