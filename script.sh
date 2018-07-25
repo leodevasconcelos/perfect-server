7b8743838c0d75ee467bfc796c958e8864e7b045
vendettafv <leonardofeitosa.v@gmail.com>
2018-07-25 19:23:28 -0300

    Update Script

--- script.sh
+++ script.sh
@@ -5,7 +5,7 @@ sudo apt-get install mysql-server
 
 echo "Instalação segura do MySQL (y/n)?"
 read ANS
-if [ "$ANS" == "y" ];
+if [ "$ANS" == "y" || "$ANS" == "Y" ];
 then
 	# Instalação segura do MySQL
 	sudo mysql_secure_installation
