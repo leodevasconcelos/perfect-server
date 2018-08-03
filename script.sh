78dcbc34f89689ad638528a14b543251eee44bea
vendettafv <leonardofeitosa.v@gmail.com>
2018-08-03 11:18:06 -0300

    Configurações Finais do Script

--- script.sh
+++ script.sh
@@ -132,26 +132,47 @@ elif [ "$SERVER" == "2" ]; then
     sudo apt install -y php5.6 php5.6-fpm php5.6-curl php5.6-gd php5.6-mbstring php5.6-mcrypt php5.6-cli php5.6-memcached php5.6-mysql php5.6-xml php5.6-xmlrpc php5.6-intl php5.6-xsl php5.6-zip
     # sudo apt-get install memcached
     # Configurações do PHP
-    
-    # /etc/php5.6/fpm/php.ini
+    # sed -i "s@^memory_limit.*@memory_limit = 512M@" /etc/php/5.6/fpm/php.ini
+    # sed -i 's@^output_buffering =@output_buffering = On\noutput_buffering =@' /etc/php/5.6/fpm/php.ini
+    sed -i 's@^;cgi.fix_pathinfo.*@cgi.fix_pathinfo=0@' /etc/php/5.6/fpm/php.ini
+    # sed -i 's@^short_open_tag = Off@short_open_tag = On@' /etc/php/5.6/fpm/php.ini
+    # sed -i 's@^expose_php = On@expose_php = Off@' /etc/php/5.6/fpm/php.ini
+    # sed -i 's@^request_order.*@request_order = "CGP"@' /etc/php/5.6/fpm/php.ini
+    sed -i 's@^;date.timezone.*@date.timezone = America/Sao_Paulo@' /etc/php/5.6/fpm/php.ini
+    sed -i 's@^post_max_size.*@post_max_size = 100M@' /etc/php/5.6/fpm/php.ini
+    sed -i 's@^upload_max_filesize.*@upload_max_filesize = 100M@' /etc/php/5.6/fpm/php.ini
+    sed -i 's@^max_execution_time.*@max_execution_time = 600@' /etc/php/5.6/fpm/php.ini
+    # sed -i 's@^;realpath_cache_size.*@realpath_cache_size = 2M@' /etc/php/5.6/fpm/php.ini
+    # sed -i 's@^disable_functions.*@disable_functions = passthru,exec,system,chroot,chgrp,chown,shell_exec,proc_open,proc_get_status,ini_alter,ini_restore,dl,openlog,syslog,readlink,symlink,popepassthru,stream_socket_server,fsocket,popen@' /etc/php/5.6/fpm/php.ini
+    # [ -ne /usr/sbin/sendmail ] && sed -i 's@^;sendmail_path.*@sendmail_path = /usr/sbin/sendmail -t -i@' /etc/php/5.6/fpm/php.ini
+    # sed -i "s@^;curl.cainfo.*@curl.cainfo = ${openssl_install_dir}/cert.pem@" /etc/php/5.6/fpm/php.ini
+    # sed -i "s@^;openssl.cafile.*@openssl.cafile = ${openssl_install_dir}/cert.pem@" /etc/php/5.6/fpm/php.ini
+    # Configurações do Nginx
+    sudo mv /etc/nginx/sites-available/default /etc/nginx/sites-available/default_config
+    sudo touch /etc/nginx/sites-available/default
+    sudo echo "server {
+    listen 80 default_server;
+    listen [::]:80 default_server;
 
-    # sed -i "s@^memory_limit.*@memory_limit = 512M@" /etc/php5.6/fpm/php.ini
-    # sed -i 's@^output_buffering =@output_buffering = On\noutput_buffering =@' /etc/php5.6/fpm/php.ini
-    sed -i 's@^;cgi.fix_pathinfo.*@cgi.fix_pathinfo=0@' /etc/php5.6/fpm/php.ini
-    # sed -i 's@^short_open_tag = Off@short_open_tag = On@' /etc/php5.6/fpm/php.ini
-    # sed -i 's@^expose_php = On@expose_php = Off@' /etc/php5.6/fpm/php.ini
-    # sed -i 's@^request_order.*@request_order = "CGP"@' /etc/php5.6/fpm/php.ini
-    sed -i 's@^;date.timezone.*@date.timezone = America/Sao_Paulo@' /etc/php5.6/fpm/php.ini
-    sed -i 's@^post_max_size.*@post_max_size = 100M@' /etc/php5.6/fpm/php.ini
-    sed -i 's@^upload_max_filesize.*@upload_max_filesize = 100M@' /etc/php5.6/fpm/php.ini
-    sed -i 's@^max_execution_time.*@max_execution_time = 600@' /etc/php5.6/fpm/php.ini
-    # sed -i 's@^;realpath_cache_size.*@realpath_cache_size = 2M@' /etc/php5.6/fpm/php.ini
-    # sed -i 's@^disable_functions.*@disable_functions = passthru,exec,system,chroot,chgrp,chown,shell_exec,proc_open,proc_get_status,ini_alter,ini_restore,dl,openlog,syslog,readlink,symlink,popepassthru,stream_socket_server,fsocket,popen@' /etc/php5.6/fpm/php.ini
-    # [ -ne /usr/sbin/sendmail ] && sed -i 's@^;sendmail_path.*@sendmail_path = /usr/sbin/sendmail -t -i@' /etc/php5.6/fpm/php.ini
-    # sed -i "s@^;curl.cainfo.*@curl.cainfo = ${openssl_install_dir}/cert.pem@" /etc/php5.6/fpm/php.ini
-    # sed -i "s@^;openssl.cafile.*@openssl.cafile = ${openssl_install_dir}/cert.pem@" /etc/php5.6/fpm/php.ini
-    # Restart Nginx
-    sudo service nginx restart
+    root /var/www/html;
+    index index.php index.html index.htm index.nginx-debian.html;
+
+    server_name localhost;
+
+    location / {
+        try_files $uri $uri/ =404;
+    }
+    location ~ \.php$ {
+           try_files $uri =404;
+           fastcgi_pass unix:/run/php/php5.6-fpm.sock;
+           fastcgi_index index.php;
+           fastcgi_param SCRIPT_FILENAME $document_root$fastcgi_script_name;
+           include fastcgi_params;
+                
+     }
+}" >> /etc/nginx/sites-available/default
+    # Restart PHP-FPM & Nginx
+    sudo systemctl restart php5.6-fpm && sudo systemctl restart nginx
   elif [ "$PHPV2" == "2" ]; then
     # Instalando PHP 7.2
     echo -e "${ORANGE}Adiconando repositório do PHP 7.2${NORMAL}"
@@ -190,7 +211,7 @@ elif [ "$SERVER" == "2" ]; then
     rewrite_log on;
 
     # The location of our projects public directory.
-    root /usr/share/nginx/html;
+    root /var/www/html;
 
     # Point index to the Laravel front controller.
     index index.php index.html;
