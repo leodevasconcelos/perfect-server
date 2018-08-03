1f1ffaa0681ecdcc5a01221413e3af29693d14fc
vendettafv <leonardofeitosa.v@gmail.com>
2018-08-03 13:17:50 -0300

    Update Script

--- script.sh
+++ script.sh
@@ -129,7 +129,7 @@ elif [ "$SERVER" == "2" ]; then
     echo -e "${ORANGE}Adiconando repositório do PHP 5.6${NORMAL}"
     sudo add-apt-repository -y ppa:ondrej/php && sudo apt-get update
 	  echo -e "${ORANGE}Instalando 5.6 + PHP-FPM${NORMAL}"
-    sudo apt install -y php5.6 php5.6-fpm php5.6-curl php5.6-gd php5.6-mbstring php5.6-mcrypt php5.6-cli php5.6-memcached php5.6-mysql php5.6-xml php5.6-xmlrpc php5.6-intl php5.6-xsl php5.6-zip
+    sudo apt-get install -y php5.6 php5.6-fpm php5.6-curl php5.6-gd php5.6-mbstring php5.6-mcrypt php5.6-cli php5.6-memcached php5.6-mysql php5.6-xml php5.6-xmlrpc php5.6-intl php5.6-xsl php5.6-zip
     # sudo apt-get install memcached
     # Configurações do PHP
     # sed -i "s@^memory_limit.*@memory_limit = 512M@" /etc/php/5.6/fpm/php.ini
@@ -173,12 +173,13 @@ elif [ "$SERVER" == "2" ]; then
 }" >> /etc/nginx/sites-available/default
     # Restart PHP-FPM & Nginx
     sudo systemctl restart php5.6-fpm && sudo systemctl restart nginx
+    sudo echo "<?php phpinfo(); ?>" >> /var/www/html/info.php
   elif [ "$PHPV2" == "2" ]; then
     # Instalando PHP 7.2
     echo -e "${ORANGE}Adiconando repositório do PHP 7.2${NORMAL}"
     sudo add-apt-repository -y ppa:ondrej/php && sudo apt-get update
 	  echo -e "${ORANGE}Instalando PHP 7.2 + PHP-FPM${NORMAL}"
-    sudo apt-get install php7.2-cli php7.2-fpm php7.2-mysql php7.2-curl php-memcached php7.2-dev php7.2-sqlite3 php7.2-mbstring php7.2-gd php7.2-json php7.2-xmlrpc php7.2-xml php7.2-zip
+    sudo apt-get install -y php7.2-cli php7.2-fpm php7.2-mysql php7.2-curl php-memcached php7.2-dev php7.2-sqlite3 php7.2-mbstring php7.2-gd php7.2-json php7.2-xmlrpc php7.2-xml php7.2-zip
     # Configurações do PHP
     # sed -i "s@^memory_limit.*@memory_limit = ${Memory_limit}M@" /etc/php/7.2/fpm/php.ini
     # sed -i 's@^output_buffering =@output_buffering = On\noutput_buffering =@' /etc/php/7.2/fpm/php.ini
@@ -247,6 +248,7 @@ elif [ "$SERVER" == "2" ]; then
 }" >> /etc/nginx/sites-available/default
     # Restart PHP-FPM & Nginx
     sudo systemctl restart php7.2-fpm && sudo systemctl restart nginx
+    sudo echo "<?php phpinfo(); ?>" >> /var/www/html/info.php
   fi
 fi
 
@@ -266,7 +268,7 @@ elif [ "$SQLSERVER" == "2" ]; then
 fi
 
 # Instalação segura do MySQL/MariaDB
-echo -ne "${GREEN}Realizar instalação segura do MySQL/MariaDB${NORMAL} [Y/n]: ?"
+echo -ne "${GREEN}Realizar instalação segura do MySQL/MariaDB?${NORMAL} [Y/n]: "
 read SGBDSECURE
 if [ "$SGBDSECURE" == "y" ] || [ "$SGBDSECURE" == "Y" ]; then
   # Instalação segura
@@ -275,7 +277,7 @@ if [ "$SGBDSECURE" == "y" ] || [ "$SGBDSECURE" == "Y" ]; then
 fi
 
 # Instalação do phpMyadmin
-echo -ne "${GREEN}Deseja instalar o phpMyAdmin${NORMAL} [Y/n]: "
+echo -ne "${GREEN}Deseja instalar o phpMyAdmin?${NORMAL} [Y/n]: "
 read PHPMYADMIN
 if [ "$PHPMYADMIN" == "y" ] || [ "$PHPMYADMIN" == "Y" ]; then
   # phpMyAdmin
@@ -300,7 +302,7 @@ if [ "$PHPMYADMIN" == "y" ] || [ "$PHPMYADMIN" == "Y" ]; then
 fi
 
 # Instalação do Composer
-echo -ne "${GREEN}Deseja instalar o Composer${NORMAL} [Y/n]: "
+echo -ne "${GREEN}Deseja instalar o Composer?${NORMAL} [Y/n]: "
 read COMPOSER
 if [ "$COMPOSER" == "y" ] || [ "$COMPOSER" == "Y" ]; then
   # Composer
